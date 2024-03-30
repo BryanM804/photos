9663eebb57c6d81a87716f3f248eca05842d4f76
BryanM804 <bmulholland41@gmail.com>
2024-03-29 20:14:51 -0400

    admin subsystem framework

---
+++ src/controller/AdminController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class AdminController {
+    
+    @FXML Button logoutButton;
+    @FXML Button createUserButton;
+    @FXML Button delUserButton;
+    @FXML TextField usernameInput;
+    @FXML ListView<String> userList;
+
+    
+}
